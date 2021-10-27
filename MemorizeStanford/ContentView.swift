a198eb913370ae7a8c2b9fe66220be0053cf8d1e
werfa1 <pasha.otverchenko@gmail.com>
2021-10-27 11:22:45 +0300

    First steps into the course

--- MemorizeStanford/ContentView.swift
+++ MemorizeStanford/ContentView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .stroke()
+                .padding()
+                .foregroundColor(.purple)
+            
+            Text("Hello, world!")
+                .padding()
+                .foregroundColor(.pink)
+        }
             .padding()
     }
 }

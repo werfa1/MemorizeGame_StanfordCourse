295f6afd842492dd38dfc9f55f7056f73160c433
werfa1 <pasha.otverchenko@gmail.com>
2021-10-27 10:29:32 +0300

    Initial Commit

---
+++ MemorizeStanford/MemorizeStanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeStanfordApp.swift
+//  MemorizeStanford
+//
+//  Created by Pavel Otverchenko on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeStanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

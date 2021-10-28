51b07840841d514d44c5b331c4a499760b31bfd5
werfa1 <pasha.otverchenko@gmail.com>
2021-10-28 15:06:35 +0300

    Lecture 2 finished

--- MemorizeStanford/ContentView.swift
+++ MemorizeStanford/ContentView.swift
@@ -8,18 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = ["👁", "👀", "🫀", "🫁", "🧠", "👄", "🦷", "👅", "👂🏽", "👃🏼", "👣", "💀", "☠️"]
+    @State var emojiCount = 10
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(emoji: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .padding()
+            }
+            
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding()
+            .font(.largeTitle)
+        }
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+struct CardView: View {
+    
+    var emoji: String
+    @State var isFaceUp : Bool = true
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 30)
-                .stroke()
-                .padding()
-                .foregroundColor(.purple)
-            
-            Text("Hello, world!")
-                .padding()
-                .foregroundColor(.pink)
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape
+                    .stroke()
+                    .fill()
+                    .foregroundColor(.purple)
+                
+                Text(emoji)
+                    .padding()
+                    .foregroundColor(.pink)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
         }
-            .padding()
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 

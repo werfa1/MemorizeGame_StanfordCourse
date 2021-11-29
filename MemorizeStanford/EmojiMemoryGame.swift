537fab720e4bb54287a01b6c638bd7a7cd4d8cfe
werfa1 <pasha.otverchenko@gmail.com>
2021-11-29 14:43:29 +0300

    Added more themes Added function to identify theme's color

--- MemorizeStanford/EmojiMemoryGame.swift
+++ MemorizeStanford/EmojiMemoryGame.swift
@@ -11,24 +11,56 @@ final class EmojiMemoryGame: ObservableObject {
     
     //MARK: - Static Properties -
     
-    static let allThemes = [Theme(id: 0,
-                                  themeName: "Body",
-                                  themeImageName: "person",
-                                  cardContent: ["👁", "👀", "🫀", "🫁", "🧠", "👄", "🦷", "👅", "👂🏽"],
-                                  numberOfPairs: 5,
-                                  color: "orange"),
-                            Theme(id: 1,
-                                  themeName: "Cars",
-                                  themeImageName: "car",
-                                  cardContent: ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚜"],
-                                  numberOfPairs: 4,
-                                  color: "purple"),
-                            Theme(id: 2,
-                                  themeName: "Animals",
-                                  themeImageName: "pawprint.fill",
-                                  cardContent: ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️"],
-                                  numberOfPairs: 3,
-                                  color: "pink")]
+    static let allThemes = [
+        Theme(
+            id: 0,
+            themeName: "Body",
+            themeImageName: "person",
+            cardContent: ["👁", "👀", "🫀", "🫁", "🧠", "👄", "🦷", "👅", "👂🏽"],
+            numberOfPairs: 9,
+            color: "orange"
+        ),
+        Theme(
+            id: 1,
+            themeName: "Cars",
+            themeImageName: "car",
+            cardContent: ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚜"],
+            numberOfPairs: 8,
+            color: "purple"
+        ),
+        Theme(
+            id: 2,
+            themeName: "Animals",
+            themeImageName: "pawprint.fill",
+            cardContent: ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️"],
+            numberOfPairs: 6,
+            color: "pink"
+        ),
+        Theme(
+            id: 3,
+            themeName: "Flowers",
+            themeImageName: "leaf",
+            cardContent: ["🌴", "🌿", "🍀", "🪴", "🍂", "🍁", "🍄", "🌾", "🌸"],
+            numberOfPairs: 7,
+            color: "green"
+        ),
+        Theme(
+            id: 4,
+            themeName: "Weather",
+            themeImageName: "cloud",
+            cardContent: ["⚡️", "☄️", "🌪", "🌈", "☀️", "☁️", "❄️", "🌧", "🌊"],
+            numberOfPairs: 9,
+            color: "yellow"
+        ),
+        Theme(
+            id: 5,
+            themeName: "Food",
+            themeImageName: "cart",
+            cardContent: ["🍏", " 🍐", "🍌", "🍓", "🍉", "🥥", "🥑", "🫒", "🍑"],
+            numberOfPairs: 5,
+            color: "blue"
+        )
+    ]
     
     //MARK: - Published Properties -
     
@@ -82,5 +114,27 @@ final class EmojiMemoryGame: ObservableObject {
     func startNewGame() {
         let newThemeIndex = Int.random(in: 0..<EmojiMemoryGame.allThemes.count)
         model.setGameTheme(EmojiMemoryGame.allThemes[newThemeIndex])
+    }
+    
+    func getThemeColor() -> Color? {
+        let currentTheme = EmojiMemoryGame.allThemes[selectedThemeIndex]
+        switch currentTheme.color {
+        case "pink" :
+            return .pink
+        case "blue":
+            return .blue
+        case "red":
+            return .red
+        case "green":
+            return .green
+        case "purple":
+            return .purple
+        case "yellow":
+            return .yellow
+        case "orange":
+            return .orange
+        default:
+            return nil
+        }
     }
 }
